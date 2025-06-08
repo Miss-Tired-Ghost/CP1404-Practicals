440685266754fdf51cbf919a949a23f2ea3395ba
Miss-Tired-Ghost <aprilofthe1st@gmail.com>
2025-06-08 15:56:47 +1000

    Add randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,27 @@
+# What did you see on line 1?*
+# 7
+# What was the smallest number you could have seen
+# 5
+# What was the largest?
+# 20
+
+# What did you see on line 2?*
+# 5
+# What was the smallest number you could have seen,
+# 3
+# What was the largest?
+# 9
+# Could line 2 have produced a 4?
+# no
+
+
+# What did you see on line 3?
+# 4.006603748040179
+# What was the smallest number you could have seen
+# 2.5
+# What was the largest?
+# 5.5
+
+#- Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.uniform(1,100))

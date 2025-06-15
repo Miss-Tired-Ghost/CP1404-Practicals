140c4f1b81bcd6aaf580089524360ecb156455d3
Miss-Tired-Ghost <aprilofthe1st@gmail.com>
2025-06-15 15:51:21 +1000

    Reformat to fit conventions

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,18 +7,29 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
+    print("")  # to make output match sample
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+    print_report(incomes)
+
+
+def print_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    # removing the extra parameter feels very good
+    # but recalculating the length feels bad
+    # is there a better way I should be doing this? (genuine question)
+    # my gut is telling me there isn't
+    for month in range(1, len(incomes) + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        # converted to fstring for readability
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
-main()
+if __name__ == "__main__":
+    main()

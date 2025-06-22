bb27cb7c63d73b3f8761f0a078dba37bc9f90c14
Miss-Tired-Ghost <aprilofthe1st@gmail.com>
2025-06-22 15:13:31 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,18 +1,30 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+# DONE?: Reformat this file so the dictionary code follows PEP 8 convention
+# I think pycharm did this automatically when I cloned the file.
+# I made the formatting incorrect, then used reformating to fix it, but this won't be detected in commits unfortunately
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
+for code, state in CODE_TO_NAME.items():
+    print(f"{code:3} is {state}")
 
-state_code = input("Enter short state: ")
+
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    # LLBY
+    # if state_code in CODE_TO_NAME:
+    #     print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
+    # else:
+    #     print("Invalid short state")
+
+    # EAFP
+    try:
+        print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+
+    state_code = input("Enter short state: ").upper()

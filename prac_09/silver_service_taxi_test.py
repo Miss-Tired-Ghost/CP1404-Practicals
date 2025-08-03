c19ab4cf9128abc599c8cdf7ab79e4dc5643e93e
Miss-Tired-Ghost <aprilofthe1st@gmail.com>
2025-08-03 22:23:03 +1000

    Complete inheriting_enhancements task

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -10,13 +10,13 @@ test_taxi = SilverServiceTaxi("Beetle", 200, 2)
 test_taxi.drive(18)
 
 assert test_taxi.__str__() == "Beetle, fuel=182, odo=18, 18km on current fare, $2.46/km plus flagfall of $4.50"
-assert test_taxi.get_fare() == 48.78
+assert test_taxi.get_fare() == 48.80
 
 
 test_taxi = SilverServiceTaxi("Hummer", 200, 4)
 test_taxi.drive(44)
 
 assert test_taxi.__str__() == "Hummer, fuel=156, odo=44, 44km on current fare, $4.92/km plus flagfall of $4.50"
-assert test_taxi.get_fare() == 220.98
+assert test_taxi.get_fare() == 221.00
 
 print("Tests passed")
